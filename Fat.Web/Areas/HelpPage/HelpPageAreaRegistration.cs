793c6df953956507f6a1f8d28fc9a3816ca8045d
benmcevoy <ben.mcevoy@gmail.com>
2013-03-15 00:10:32 +1100

    oh, i need these files too...

---
+++ Fat.Web/Areas/HelpPage/HelpPageAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Fat.Web.Areas.HelpPage
+{
+    public class HelpPageAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "HelpPage";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "HelpPage_default",
+                "HelpPage/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
